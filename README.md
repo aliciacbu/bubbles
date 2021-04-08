16a7cae5a03b1c8ef496628bc7dd2a6ec0e61315
= <aliciacbu@gmail.com>
2021-04-08 22:43:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bubbles graph with D3.js v4
+![v4](screenshot/bubblesv4.PNG)
+*Screenshot*
---
+++ js/bubbles.js
@@ -0,0 +1,109 @@
+// set the dimensions and margins of the graph
+var width = 500
+var height = 500
+
+// append the svg object to the body of the page
+var svg = d3.select("#bubbles")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+// Read data
+d3.json("data/data.json", function(data) {
+  // Filter a bit the data -> more than 1 million inhabitants
+  data = data.filter(function(d){ return d.total_cartes })
+
+
+    var color = d3.scaleOrdinal()
+    .domain(function(d){ return d.id_familia})
+    .range(d3.schemeTableau10);
+
+
+  // Size scale for countries
+  var size = d3.scaleLinear()
+    .domain([0, 200])
+    .range([7,100])  // circle will be between 7 and 55 px wide
+
+  // create a tooltip
+  var Tooltip = d3.select("#bubbles")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html('<strong>' + d.nom + '</strong>' + " (Família " + d.nom_familia + ") <br>" + d.total_cartes + " cartes")
+      .style("left", (d3.mouse(this)[0]+20) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+  }
+
+  // Initialize the circle: all located at the center of the svg area
+  var node = svg.append("g")
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("class", "node")
+      .attr("r", function(d){ return size(d.total_cartes)})
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .style("fill", function(d){ return color(d.id_familia)})
+      //.style("fill", color)
+      .style("fill-opacity", 0.8)
+      .attr("stroke", "black")
+      .style("stroke-width", 1)
+      .on("mouseover", mouseover) // What to do when hovered
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+      .call(d3.drag() // call specific function when circle is dragged
+           .on("start", dragstarted)
+           .on("drag", dragged)
+           .on("end", dragended));
+
+  // Features of the forces applied to the nodes:
+  var simulation = d3.forceSimulation()
+      .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+      .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+      .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.total_cartes)+3) }).iterations(1)) // Force that avoids circle overlapping
+
+  // Apply these forces to the nodes and update their positions.
+  // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+  simulation
+      .nodes(data)
+      .on("tick", function(d){
+        node
+            .attr("cx", function(d){ return d.x; })
+            .attr("cy", function(d){ return d.y; })
+      });
+
+  // What happens when a circle is dragged?
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(.03).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(.03);
+    d.fx = null;
+    d.fy = null;
+  }
+
+})
